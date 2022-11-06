6bd9793379a9ad5020c4511addd11d9fe003dd0f
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-11-06 17:03:13 +0200

    Create MyHwApplication.java

---
+++ myHW/src/main/java/com/example/myHW/MyHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.myHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHwApplication.class, args);
+	}
+
+}
