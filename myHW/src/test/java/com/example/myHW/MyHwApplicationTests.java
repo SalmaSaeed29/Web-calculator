08f9839112c1b3130008c91077c57c38fd792298
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-11-06 17:11:16 +0200

    Create MyHwApplicationTests.java

---
+++ myHW/src/test/java/com/example/myHW/MyHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
